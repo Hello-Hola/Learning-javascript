99020f624d166d471c6f55d51b0e98b6978f292b
Asif suhail khan <asifsuhailkhan1@gmail.com>
2023-07-19 22:10:52 +0530

    I am so happy to tell that i completed my first number incrementing game , hip - hip hurry

---
+++ Java script/js-learning-days/day02/lapCounter.js
@@ -0,0 +1,14 @@
+
+// run lap counter increment funciton only theree times;
+
+let lapsCompleted = 0;
+
+function increment(){
+    lapsCompleted += 4;
+}
+increment()
+increment()
+increment()
+
+console.log(lapsCompleted);
+
---
+++ Java script/js-projects/noIncrement/script.js
@@ -0,0 +1,12 @@
+
+let number = document.querySelector('#number');
+// console.log(number);
+let count = 0;
+function increment(){
+    count ++;
+    number.innerHTML = count;
+    // console.log(count);
+}
+
+
+
